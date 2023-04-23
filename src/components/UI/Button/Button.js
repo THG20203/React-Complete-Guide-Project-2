bd17e650afdaa1b4d6fbbf1afdde2c33ae4e87f1
THG20203 <tristanhgriffiths8@yahoo.com>
2023-04-23 23:30:38 +0100

    tagged template literal intro

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
+/* use styled components package since we just installed it */
 
-import './Button.css';
+const Button = styled.button``;
+/* the two back tickets after button -> a tagged template literal. Its a default 
+JavaScript feature, its not specific to this package and its not specific to react. */
+/* button is simply a method on this styled object. Styled is an object we're importing
+from styled components - there we can access the button method. */
+/* Instead of parentheses, I've added backticks */
 
-const Button = props => {
-  return (
-    <button type={props.type} className="button" onClick={props.onClick}>
-      {props.children}
-    </button>
-  );
-};
+// const Button = props => {
+//   return (
+//     <button type={props.type} className="button" onClick={props.onClick}>
+//       {props.children}
+//     </button>
+//   );
+// };
 
 export default Button;
